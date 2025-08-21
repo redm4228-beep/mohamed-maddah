ec3f1db0e7ce784ecfdadab532249ee17357f389
redm4228-beep <redm4228@gmail.com>
2025-08-21 03:36:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// تغيير لون الناف بار عند التمرير
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
